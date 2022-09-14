ec5e9d2f4c85b8250e5954be019d1effafc6153e
Matilda Mared <matildamared@live.se>
2022-09-14 10:13:03 +0200

    add simple server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-"use strict";
+import { Request, Response } from "express";
+const express = require("express");
+const PORT = 3000;
 
-console.log("Hello world");
+const app = express();
+
+app.use("/", (req: Request, res: Response) => {
+	res.send("Hello World!");
+});
+
+app.listen(PORT || 3000, () => {
+	console.log(`Server up and running on port ${PORT}... 🌈`);
+});
